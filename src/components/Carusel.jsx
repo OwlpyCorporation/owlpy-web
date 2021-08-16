6cb3556de5e7d20e264f0b62c2b4c03e7eeb991e
OwlpyCorporation <OwlpyCorporation@gmail.com>
2021-08-16 12:32:41 -0500

    Correcciones e integracion de imagenes

--- src/components/Carusel.jsx
+++ src/components/Carusel.jsx
@@ -1,13 +1,31 @@
 import { Fragment } from "react";
+import useWindowDimensions from "./DimensionsWindow";
+import img from "../assets/images/imagen.png";
+import imgToGrid from "../assets/images/imagenToGrid.png";
+import firstIconToGrid from "../assets/images/firstIconToGrid.png";
+import secondIconToGrid from "../assets/images/secondIconToGrid.png";
+import thirdIconToGrid from "../assets/images/thirdIconToGrid.png";
+
 export default function Carusel() {
-  const img = "https://source.unsplash.com/random";
+  const { width } = useWindowDimensions();
+  const changeImageAcordingToWindowSize = (
+    maxWidthParam,
+    firstImage,
+    secondImage
+  ) => {
+    if (width >= maxWidthParam) {
+      return firstImage;
+    } else {
+      return secondImage;
+    }
+  };
   const img2 = "https://source.unsplash.com/random";
   const img3 = "https://source.unsplash.com/random";
 
   return (
     <Fragment>
       <div className="row bg-blue-700 justify-content-center mb-3">
-        <div className="col-lg-10 col-sm-auto p-0">
+        <div className="col-lg-10 col-sm-0 p-0">
           <div
             id="carouselExampleInterval"
             className="carousel slide"
@@ -16,7 +34,7 @@ export default function Carusel() {
             <div className="carousel-inner">
               <div className="carousel-item active" data-bs-interval="2000">
                 <img
-                  src={img}
+                  src={changeImageAcordingToWindowSize(1300, img, imgToGrid)}
                   className="rounded mx-auto d-block w-100"
                   alt="..."
                   width="640"
@@ -25,7 +43,11 @@ export default function Carusel() {
               </div>
               <div className="carousel-item" data-bs-interval="2000">
                 <img
-                  src={img2}
+                  src={changeImageAcordingToWindowSize(
+                    1300,
+                    img2,
+                    firstIconToGrid
+                  )}
                   className="rounded mx-auto d-block w-100"
                   alt="..."
                   width="620"
@@ -34,7 +56,24 @@ export default function Carusel() {
               </div>
               <div className="carousel-item">
                 <img
-                  src={img3}
+                  src={changeImageAcordingToWindowSize(
+                    1300,
+                    img3,
+                    secondIconToGrid
+                  )}
+                  className="rounded mx-auto d-block w-100"
+                  alt="..."
+                  width="620"
+                  height="450"
+                />
+              </div>
+              <div className="carousel-item">
+                <img
+                  src={changeImageAcordingToWindowSize(
+                    1300,
+                    img3,
+                    thirdIconToGrid
+                  )}
                   className="rounded mx-auto d-block w-100"
                   alt="..."
                   width="620"
@@ -67,6 +106,7 @@ export default function Carusel() {
               <span className="visually-hidden">Next</span>
             </button>
           </div>
+          <h1>{width}</h1>
         </div>
       </div>
     </Fragment>
